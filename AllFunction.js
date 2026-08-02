125e60b0d9d13fe3bfcb73c4e8cfdbcb11c3abab
Shojjoti <shojjotihhossenbd30@gmail.com>
2026-08-03 03:00:04 +0600

    All function together

--- AllFunction.js
+++ AllFunction.js
@@ -75,3 +75,26 @@ function topRatedRestaurant(restaurants) {
 
 //PROBLEM 5
 
+function averageResponseTime(times) {
+  if (Array.isArray(times) === false){    
+    return "Invalid";
+  }
+   
+  if (times.length === 0) {
+      return "Invalid";
+  }
+
+  for (let i = 0; i < times.length; i++) {
+    if (typeof times[i] !== "number" || isNaN(times[i])) {
+    return "Invalid";
+    }
+  }
+   
+  let total = 0;
+  for (let i = 0; i < times.length; i++) {
+    total = total + times[i];
+  }
+  
+  return total / times.length; 
+}
+
